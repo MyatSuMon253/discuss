3c592f65cf61c020566ed65780cd7a49eb4fa83a
msm <myatsumon@ucsy.edu.mm>
2024-02-25 22:11:50 +0630

    feat: static caching while using auth

---
+++ app/components/header-auth.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import * as actions from "@/app/actions";
+import {
+  Avatar,
+  Button,
+  NavbarItem,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+
+const HeaderAuth = () => {
+  const session = useSession();
+
+  let authContent: React.ReactNode;
+  if (session.status === "loading") {
+    authContent = null;
+  } else if (session?.data?.user) {
+    authContent = (
+      <Popover placement="left">
+        <PopoverTrigger>
+          <Avatar src={session?.data.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign In
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return authContent;
+};
+
+export default HeaderAuth;
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,5 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/app/actions";
-import { auth } from "@/app/auth";
-import Profile from "@/app/components/profile";
+const Home = () => {
+  return <div>Home</div>;
+};
 
-export default async function Home() {
-  const session = await auth();
-
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign In</Button>
-      </form>
-
-      <form action={actions.signOut}>
-        <Button type="submit">Sign Out</Button>
-      </form>
-
-      {session?.user ? (
-        <div>{JSON.stringify(session.user)}</div>
-      ) : (
-        <div>Signed Out</div>
-      )}
-
-      <Profile />
-    </div>
-  );
-}
+export default Home;
